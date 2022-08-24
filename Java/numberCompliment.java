36dc55a7592d72788ff212c8c29abe1db951ac9d
Rohit Singh <sirohit328@gmail.com>
2022-08-24 10:06:17 +0530

    Create numberCompliment.java

---
+++ Java/numberCompliment.java
@@ -0,0 +1,15 @@
+// Leet Code Question No. 476. Number Complement
+// Link : https://leetcode.com/problems/number-complement/
+
+// Created By Rohit Singh on 24/08/2022
+
+class Solution {
+    public int findComplement(int num) {
+        int mask = 1;
+        while (mask<num){
+            mask = (mask<<1)+1;
+        }
+        int ans = num^mask;
+        return ans;
+    }
+}
